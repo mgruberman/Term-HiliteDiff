c21f204f43825eca2d79b94469789f4c9ad6d99b
Josh ben Jore <jbenjore@whitepages.com>
2009-12-13 14:51:22 -0800

    0.02

--- lib/Term/HiliteDiff.pm
+++ lib/Term/HiliteDiff.pm
@@ -1,150 +1,41 @@
 package Term::HiliteDiff;
 
 use strict;
-use vars qw( $VERSION @EXPORT_OK %EXPORT_TAGS $DEFAULTOBJ );
+use vars qw( $VERSION @EXPORT_OK %EXPORT_TAGS );
 use Term::HiliteDiff::_impl ();
 
-=head1 NAME
-
-Term::HiliteDiff - Hilites differences in text with ANSI escape codes
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-$VERSION = '0.01';
-
-=head1 DESCRIPTION
-
-=head1 SYNOPSIS
-
-This module hilites differences between lines/records of text. It was
-directly inspired by the --difference mode provided by the watch(1)
-program on Linux.
-
-    use Term::HiliteDiff 'watch';
-    for my $num ( 0 .. 1_000_000 ) {
-        print watch( $num ), "\n";
-    }
-
-=head1 EXPORT
-
-This module optionally exports C<watch> and C<hilite_diff> under most
-circumstances. When perl is invoked as a one-liner with C<-e>, both
-C<watch> and C<hilite_diff> are exported to the C<main> package.
-
-=cut
+$VERSION = '0.02';
 
 use Exporter ();
-
 *import      = \&Exporter::import;
 @EXPORT_OK   = qw( watch hilite_diff );
 %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 # Auto-export everything to main if I've been called as a -e program.
 if ( $0 eq '-e' ) {
-    no strict 'refs';
-    *{"main::$_"} = \&$_ for @EXPORT_OK;
+
+    package main;
+    Term::HiliteDiff->import(':all');
 }
 
-=head1 FUNCTIONS
-
-=head2 hilite_diff
-
- $str = hilite_diff( LIST );
- $str = hilite_diff();
- hilite_diff( LIST );
- hilite_diff();
-
-=cut
+# Here are some convenience functions for pretending this module isn't
+# object oriented.
+my $DEFAULTOBJ = __PACKAGE__->new;
 
 sub hilite_diff {
     return $DEFAULTOBJ->hilite_diff(@_);
 }
 
-=head2 watch
-
- $str = watch( LIST );
- $str = watch();
- watch( LIST );
- watch();
-
-=cut
-
 sub watch {
     return $DEFAULTOBJ->watch(@_);
 }
 
-=head1 Object oriented
-
-=head2 C<Term::HiliteDiff->new()>
-
-This class method returns a new C<Term::HiliteDiff> object.
-
-=cut
-
-$DEFAULTOBJ = __PACKAGE__->new;
-
+# Hey, a class constructor.
 sub new {
-    my $class = shift;
+    my $class = shift @_;
 
     return bless [], "${class}::_impl";
 }
 
-=head2 $obj->hilite_diff
-
-=head2 $obj->watch
-
-=head1 AUTHOR
-
-Joshua ben Jore, C<< <twists@gmail.com> >>
-
-=head1 TODO
-
-=over 4
-
-=item Objects
-
-This uses a single process global state for tracking differences. It
-should use objects so multiple things can be watched.
-
-=item Pluggable drivers
-
-Pluggable drivers. hilite_diff() is essentially exactly the same as
-watch() but it edits the marked up text differently. I'd be good to
-offer a unified diff and HTML rendering as well.
-
-It'd also be nice to have an API that would allow decaying changes to
-be visible. So a change in one line is somehow visible N lines into
-the future.
-
-=item Tests
-
-I totally skipped those. I know how I'm going to generate them, I just
-haven't yet.
-
-=back
-
-=head1 BUGS
-
-Please report any bugs or feature requests to
-C<bug-term-hilitediff@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Term-HiliteDiff>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2006 Joshua ben Jore, All Rights Reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
-
 # This quote is blatantly copied from Michael Poe of errantstory.com.
-"What's the point of dreaming I'm a girl if I don't get a cool lesbian scene?!"
+q[What's the point of dreaming I'm a girl if I don't get a cool lesbian scene?!]
--- lib/Term/HiliteDiff/_impl.pm
+++ lib/Term/HiliteDiff/_impl.pm
@@ -1,9 +1,10 @@
 package Term::HiliteDiff::_impl;
 use strict;
 use Algorithm::Diff ();
+use Carp 'carp';
 
-use constant CHARS => 0;
-use constant LINES => 1;
+use constant TOKENS => 0;
+use constant LINES  => 1;
 
 # This library uses the following ANSI escape codes
 # \e[s   Save a cursor position
@@ -12,24 +13,13 @@ use constant LINES => 1;
 # \e[7m  Turns on 'reverse' hilite
 # \e[0m  Turns off all formatting
 
-=head1 NAME
-
-Term::HiliteDiff::_impl - private implementations for methods
-
-=head1 PRIVATE METHODS
-
-=over 4
-
-=item ->hilite_diff
-
-=cut
-
 sub hilite_diff {
-    my $self = shift;
+    my $self = shift @_;
 
     # Call the normal watch() function but remove some escape
     # sequences.
     local $_ = $self->watch(@_);
+    $_ = '' unless defined;
 
     # Remove \e[s and \e[u. These functions save and restore the
     # cursor position.
@@ -45,34 +35,35 @@ sub hilite_diff {
     }
     else {
         print
-            or warn "Couldn't write to the currently selected filehandle: $!";
+            or carp "Couldn't write to the currently selected filehandle: $!";
     }
 }
 
-=item ->watch
-
-=cut
-
 sub watch {
-    my $self = shift;
-
-    # I'm mimicing print(...) so it makes sense to join things with
-    # $,. That's usually undef so I'm turning off the uninitialized
-    # warning. If there are no parameters, I read from $_ instead.
-    BEGIN { $^W = 0 }
-    my $string = (
-        @_
-        ? join( $,, @_ )
-        : $_
-    );
+    my $self = shift @_;
 
     # This is the stream of data I'll be using to compare against for
     # differences.
-    my @chars = split //, $string;
+    my ( @tokens, $string );
+    if ( eval { @tokens = @{ $_[0] }; 1 } ) {
+
+        # Allow accepting a reference to an array of pre-parsed
+        # tokens.
+        $string = join( defined($,) ? $, : '', @tokens );
+    }
+    else {
+
+        # I'm mimicing print(...) so it makes sense to join things
+        # with $,. That's usually undef so I'm turning off the
+        # uninitialized warning. If there are no parameters, I read
+        # from $_ instead.
+        $string = join( defined($,) ? $, : '', @_ );
+        @tokens = $string =~ /(.)/sg;
+    }
 
     # *if* I haven't started a diff already, then I start one and
     # finish early.
-    unless ( $self->[CHARS] ) {
+    unless ( $self->[TOKENS] ) {
 
         # \e[s instructs the terminal to save the cursor
         # position. I'll use this later with \e[u to restore the
@@ -84,18 +75,27 @@ sub watch {
         # many lines there are. I'll need to know how many lines there
         # are so I can make sure to clear them all and not leave
         # trailing bits around.
-        $self->[CHARS] = \@chars;
+        $self->[TOKENS] = \@tokens;
         $self->[LINES] = $string =~ tr/\n/\n/;
 
-        return $string;
+        # Do something sane if called in void or not-void contexts.
+        if ( defined wantarray ) {
+            return $string;
+        }
+        else {
+            print $string
+                or carp
+                "Couldn't write to the currently selected filehandle: $!";
+            return;
+        }
     }
 
     # Find out what changed between the previous call and this
     # one. Once I have the diff, I don't need to keep my copy of the
     # previous stream. I'll just be querying $diff to see what's
     # different or unchanged.
-    my $diff = Algorithm::Diff->new( \@chars, $self->[CHARS] );
-    $self->[CHARS] = \@chars;
+    my $diff = Algorithm::Diff->new( \@tokens, $self->[TOKENS] );
+    $self->[TOKENS] = \@tokens;
 
     # \e[u instructs the terminal to restore the cursor position to
     # whatever was last saved with \e[s.
@@ -104,19 +104,16 @@ sub watch {
     # Loop over each hunk in $diff. I'm looking for hunks that are the
     # same or thins that appear in the new stream, sequence #1. When
     # I've found differences, I'll mark it up.
-    #
-    # Note, splice(@items) is an optimization hack. It allows perl to
-    # avoid making copies of the elements in @items.
     while ( $diff->Next ) {
         my @items;
         if ( @items = $diff->Same ) {
-            $out .= join "", splice @items;
+            $out .= join '', splice @items;
         }
         elsif ( @items = $diff->Items(1) ) {
 
             # \e[7m adds the reverse text mode. \e[0m clears all text
             # attributes.
-            $out .= "\e[7m" . join( "", splice @items ) . "\e[0m";
+            $out .= "\e[7m" . join( '', splice @items ) . "\e[0m";
         }
     }
 
@@ -124,11 +121,11 @@ sub watch {
     # else remaining on that line. Remember, I'm writing over an area
     # on the screen that already has data in it. I want to clean
     # things up so only what I'm printing now is visible.
-    $out =~ s/(?=\n)/\e[K/g;
+    $out =~ s/\n/\e[K\n/g;
 
     # Count the # of lines that aren't in this output that were in the
     # previous output. I'll need to clear those out.
-    my $this_lines = $out =~ tr/\n/\n/;
+    my $this_lines = $out =~ tr/\n//;
     my $slack_lines = $self->[LINES] - $this_lines;
     if ( $slack_lines > 0 ) {
 
@@ -145,18 +142,14 @@ sub watch {
     }
     $self->[LINES] = $this_lines;
 
-    # Do something sane if called void or not-void contexts.
+    # Do something sane if called in void or not-void contexts.
     if ( defined wantarray ) {
         return $out;
     }
     else {
         print $out
-            or warn "Couldn't write to the currently selected filehandle: $!";
+            or carp "Couldn't write to the currently selected filehandle: $!";
     }
 }
 
-=back
-
-=cut
-
-"Wow, you're pretty uptight for a guy who worships a multi-armed, hermaphrodite embodiment of destruction who has a fetish for vaguely phallic shaped headgear.";
+q[Wow, you're pretty uptight for a guy who worships a multi-armed, hermaphrodite embodiment of destruction who has a fetish for vaguely phallic shaped headgear.];
