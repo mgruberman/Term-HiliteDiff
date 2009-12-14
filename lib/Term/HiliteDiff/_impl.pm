68e061197464a664aa238ef9d045eebc6645ebad
Josh ben Jore <jbenjore@whitepages.com>
2009-12-13 18:15:51 -0800

    Work correctly!

--- lib/Term/HiliteDiff/_impl.pm
+++ lib/Term/HiliteDiff/_impl.pm
@@ -5,6 +5,7 @@ use Carp ();
 
 use constant TOKENS => 0;
 use constant LINES  => 1;
+use constant _do_positioning => 2;
 
 # This library uses the following ANSI escape codes
 # \e[s   Save a cursor position
@@ -14,69 +15,73 @@ use constant LINES  => 1;
 # \e[0m  Turns off all formatting
 
 sub hilite_diff {
-    my $self = shift @_;
+    my ( $self, @args ) = @_;
 
-    # Call the normal watch() function but remove some escape
-    # sequences.
-    local $_ = $self->watch(@_);
-    $_ = '' unless defined;
+    local $self->[_do_positioning] = 0;
 
-    # Remove \e[s and \e[u. These functions save and restore the
-    # cursor position.
-    s/\e\[[us]//g;
-
-    # Remove the \e[K I added at the end of every line. This isn't
-    # strictly necessary.
-    s/(?:\n\e\[K)+\z//g;
-
-    # Do something sane if called void or not-void contexts.
-    if ( defined wantarray ) {
-        return $_;
-    }
-    else {
-        print
-            or carp "Couldn't write to the currently selected filehandle: $!";
-    }
+    return $self->_do_watch( @args );
 }
 
 sub watch {
+    my ( $self, @args ) = @_;
+    local $self->[_do_positioning] = 1;
+
+    return $self->_do_watch( @args );
+}
+
+sub _do_watch {
     my $self = shift @_;
 
+    # I'm mimicing print(...) so it makes sense to join things
+    # with $,. That's usually undef so I'm turning off the
+    # uninitialized warning. If there are no parameters, I read
+    # from $_ instead.
+    my $separator =
+        defined $,
+        ? $,
+        : '';
+
     # This is the stream of data I'll be using to compare against for
-    # differences.
+    # differences. Either the user provided pre-parsed data or I'm
+    # going to just compare each character.
     my ( @tokens, $string );
     if ( eval { @tokens = @{ $_[0] }; 1 } ) {
 
         # Allow accepting a reference to an array of pre-parsed
         # tokens.
-        $string = join( defined($,) ? $, : '', @tokens );
+        $string = join $separator, @tokens;
     }
     else {
 
-        # I'm mimicing print(...) so it makes sense to join things
-        # with $,. That's usually undef so I'm turning off the
-        # uninitialized warning. If there are no parameters, I read
-        # from $_ instead.
-        $string = join( defined($,) ? $, : '', @_ );
-        @tokens = $string =~ /(.)/sg;
+        $string = join $separator, @_;
+        @tokens = split //, $string;
     }
 
     # *if* I haven't started a diff already, then I start one and
     # finish early.
-    unless ( $self->[TOKENS] ) {
+    if ( ! $self->[TOKENS] ) {
 
         # \e[s instructs the terminal to save the cursor
         # position. I'll use this later with \e[u to restore the
         # position back to here and then write over whatever it was
         # that I just wrote.
-        $string = "\e[s$string\n";
+        if ( $self->[_do_positioning] ) {
+            $string = "\e[s$string";
+
+	    # For an unknown reason, ensure there's a newline
+	    if ( $string !~ /\n/ ) {
+		$string .= "\n";
+	    }
+        }
 
         # Make notes for the next run on what I just printed and how
         # many lines there are. I'll need to know how many lines there
         # are so I can make sure to clear them all and not leave
         # trailing bits around.
         $self->[TOKENS] = \@tokens;
-        $self->[LINES] = $string =~ tr/\n/\n/;
+        if ( $self->[_do_positioning] ) {
+            $self->[_cnt_lines] = $string =~ tr/\n/\n/;
+        }
 
         # Do something sane if called in void or not-void contexts.
         if ( defined wantarray ) {
@@ -96,12 +101,20 @@ sub watch {
     my $diff = Algorithm::Diff->new( \@tokens, $self->[TOKENS] );
     $self->[TOKENS] = \@tokens;
 
-    # \e[u instructs the terminal to restore the cursor position to
-    # whatever was last saved with \e[s.
-    my $out = "\e[u";
+    my $out;
+
+    if ( $self->[_do_positioning] ) {
+
+        # \e[u instructs the terminal to restore the cursor position
+        # to whatever was last saved with \e[s.
+        $out = "\e[u";
+    }
+    else {
+        $out = '';
+    }
 
     # Loop over each hunk in $diff. I'm looking for hunks that are the
-    # same or thins that appear in the new stream, sequence #1. When
+    # same or things that appear in the new stream, sequence #1. When
     # I've found differences, I'll mark it up.
     while ( $diff->Next ) {
         my @items;
@@ -112,34 +125,52 @@ sub watch {
 
             # \e[7m adds the reverse text mode. \e[0m clears all text
             # attributes.
-            $out .= "\e[7m" . join( '', splice @items ) . "\e[0m";
+	    my $new_section = "\e[7m" . join( '', splice @items ) . "\e[0m";
+
+	    # Turn off highlighting when going over a new line
+	    $new_section =~ s/\n/\e\[0m\n\e\[7m/g;
+
+	    # Remove empty highlighting
+	    $new_section =~ s/\e\[7m\e\[0m//g;
+
+            $out .= $new_section;
         }
     }
 
-    # Just before every \n, add an instruction \e[K to delete anything
-    # else remaining on that line. Remember, I'm writing over an area
-    # on the screen that already has data in it. I want to clean
-    # things up so only what I'm printing now is visible.
-    $out =~ s/\n/\e[K\n/g;
+    if ( $self->[_do_positioning] ) {
+	# Just before every \n, add an instruction \e[K to delete anything
+	# else remaining on that line. Remember, I'm writing over an area
+	# on the screen that already has data in it. I want to clean
+	# things up so only what I'm printing now is visible.
+        $out =~ s/\n/\e[K\n/g;
+
+	# Kill the remainder of the last line if we're ending in a
+	# newline.
+	$out =~ s/\n\z/\n\e[K/;
+    }
 
     # Count the # of lines that aren't in this output that were in the
     # previous output. I'll need to clear those out.
-    my $this_lines = $out =~ tr/\n//;
-    my $slack_lines = $self->[LINES] - $this_lines;
-    if ( $slack_lines > 0 ) {
+    if ( $self->[_do_positioning] ) {
+        my $this_cnt_lines = $out =~ tr/\n//;
+        my $slack_cnt_lines = $self->[_cnt_lines] - $this_cnt_lines;
+        $self->[_cnt_lines] = $this_cnt_lines;
 
-        # Add $slack_lines # of blank lines with instructions to clear
-        # things out. I'm wondering slightly if it isn't wrong to do
-        # \n before \e[K. \e[K instructs the terminal to delete
-        # everything else on a line after the current position. \n
-        # instructs the terminal to move the cursor to the beginning
-        # of the next line.
-        #
-        # I'm not sure why I have 1+ here. I must have needed but the
-        # logic doesn't sound right.
-        $out .= "\n\e[K" x ( 1 + $slack_lines );
+        if ( $slack_cnt_lines > 0 ) {
+
+            # Add $slack_cnt_lines number of blank lines with
+            # instructions to clear things out. I'm wondering slightly
+            # if it isn't wrong to do \n before \e[K. \e[K instructs
+            # the terminal to delete everything else on a line after
+            # the current position. \n instructs the terminal to move
+            # the cursor to the beginning of the next line.
+            #
+            # I'm not sure why I have 1+ here. I must have needed but the
+            # logic doesn't sound right.
+	    ++ $slack_cnt_lines;
+            $out .= "\n\e[K" x $slack_cnt_lines;
+        }
     }
-    $self->[LINES] = $this_lines;
 
     # Do something sane if called in void or not-void contexts.
     if ( defined wantarray ) {
