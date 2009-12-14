746920ba8fdc61403524984d23758756f4df54cb
Josh ben Jore <jjore@cpan.org>
2009-12-13 18:36:54 -0800

    0.06

--- lib/Term/HiliteDiff.pm
+++ lib/Term/HiliteDiff.pm
@@ -4,7 +4,7 @@ use strict;
 use vars qw( $VERSION @EXPORT_OK %EXPORT_TAGS $DEFAULTOBJ );
 use Term::HiliteDiff::_impl ();
 
-$VERSION = '0.05';
+$VERSION = '0.06';
 
 use Exporter ();
 *import      = \&Exporter::import;
