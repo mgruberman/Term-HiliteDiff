cc702f0c2550a4dcac343ab26106a6871409ef35
Josh ben Jore <jjore@cpan.org>
2009-12-16 21:55:45 -0800

    Add Test::IsEscapes::esc and isaq

--- t/lib/Test/IsEscapes.pm
+++ t/lib/Test/IsEscapes.pm
@@ -2,23 +2,49 @@ package Test::IsEscapes;
 use strict;
 use vars qw( @EXPORT_OK );
 
+use Test::More ();
 use Exporter ();
 *import = \ &Exporter::import;
 
-@EXPORT_OK = qw( isq );
+@EXPORT_OK = qw( isq isaq );
+
+sub esc {
+    my ( $val ) = @_;
+
+    $val =~ s/\n/\\n/g;
+    $val =~ s/\e/\\e/g;
+    $val =~ s/\t/\\t/g;
+
+    return $val;
+}
 
 sub isq ($$;$) {
-    my $got      = $_[0];
-    my $expected = $_[1];
+    my $got      = esc( $_[0] );
+    my $expected = esc( $_[1] );
     my $name     = $_[2];
 
-    for ( $got, $expected ) {
-        s/\n/\\n/g;
-        s/\e/\\e/g;
-        s/\t/\\t/g;
+    Test::More::is( $got, $expected, $name );
+}
+
+sub isaq ($$;$) {
+
+    my $got;
+    if ( 'ARRAY' eq ref $_[0] ) {
+        $got = [
+            map { esc( $_ ) }
+            @{$_[0]}
+        ];
+    }
+    else {
+        $got = $_[0];
     }
 
-    &main::is( $got, $expected, $name );
+    my $expected = [
+        map { esc( $_ ) }
+        @{$_[1]}
+    ];
+
+    Test::More::is_deeply( $got, $expected, $_[2] );
 }
 
 1;
